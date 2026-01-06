19936efc612d24f6192e389cd2344e196dc70f54
dediksarmawan-star <dediksarmawan@gmail.com>
2026-01-06 22:07:44 +0800

    memperbarui index pada halaman polling

--- polling/index.php
+++ polling/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include '../layouts/header.php';
 include '../config/Database.php';
 include '../classes/Polling.php';
@@ -9,28 +10,42 @@ $data = $polling->all();
 ?>
 
 <div class="container">
-<h2>Daftar Polling</h2>
-<a href="create.php"><button>Buat Polling</button></a>
+    <h2>Daftar Polling</h2>
 
-<table>
-<tr>
-    <th>Judul</th>
-    <th>Status</th>
-    <th>Aksi</th>
-</tr>
+    <div class="action-buttons">
+        <a href="create.php" class="btn btn-primary">Buat Polling</a>
+        <a href="hasil.php" class="btn btn-secondary">Hasil Vote</a>
+    </div>
 
-<?php while ($row = $data->fetch(PDO::FETCH_ASSOC)) { ?>
-<tr>
-    <td><?= $row['judul']; ?></td>
-    <td>
-        <?= (strtotime($row['end_date']) < time()) ? "Ditutup" : "Aktif"; ?>
-    </td>
-    <td>
-        <a href="detail.php?id=<?= $row['id']; ?>">Detail</a>
-    </td>
-</tr>
-<?php } ?>
-</table>
+    <table>
+        <tr>
+            <th>Judul</th>
+            <th>Status</th>
+            <th>Aksi</th>
+        </tr>
+
+        <?php while ($row = $data->fetch(PDO::FETCH_ASSOC)) { ?>
+            <tr>
+                <td><?= htmlspecialchars($row['judul']); ?></td>
+                <td>
+                    <?= (strtotime($row['end_date']) < time()) ? "Ditutup" : "Aktif"; ?>
+                </td>
+                <td class="action-cell">
+                    <a href="detail.php?id=<?= $row['id']; ?>" class="btn btn-detail">Detail</a>
+
+                    <?php if (isset($_SESSION['user_id']) && $_SESSION['user_id'] == $row['user_id']) { ?>
+                        <a href="edit.php?id=<?= $row['id']; ?>" class="btn btn-edit">Edit</a>
+
+                        <a href="delete.php?id=<?= $row['id']; ?>"
+                           class="btn btn-danger"
+                           onclick="return confirm('Yakin ingin menghapus polling ini?')">
+                           Hapus
+                        </a>
+                    <?php } ?>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
 </div>
 
 <?php include '../layouts/footer.php'; ?>
