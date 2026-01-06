7c92e1ff0b48c34de66fdc6255267758660a262e
Wibawa12 <wibawapemaron12345678910@gmail.com>
2026-01-06 22:37:29 +0800

    memperbaiki session start pada header dan footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,15 +1,5 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistem Voting Online</title>
-    <link rel="stylesheet" href="../assets/style.css">
-</head>
-<body>
-    <footer>
-       <span>&copy; <?= date('Y'); ?> Sistem Voting Online</span>
-    </footer>
+<div class="footer">
+    &copy; <?= date('Y'); ?> Sistem Voting Online
+</div>
 </body>
 </html>
