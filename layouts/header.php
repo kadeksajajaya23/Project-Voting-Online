c30b499cb59143c4d99f9dfee7de71da17829fd9
Wibawa12 <wibawapemaron12345678910@gmail.com>
2026-01-06 22:40:38 +0800

    memperbaiki code header

--- layouts/header.php
+++ layouts/header.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <title>Sistem Voting Online</title>
@@ -12,19 +12,17 @@ if (session_status() === PHP_SESSION_NONE) {
 </head>
 <body>
 
-<header>
+<header class="header">
     <div class="container">
         <h1>Sistem Voting Online</h1>
         <nav>
-            <a href="/index.php">Home</a>
-            <a href="/polling/index.php">Polling</a>
+            <a href="../index.php">Home</a>
+            <a href="../polling/index.php">Polling</a>
             <?php if (isset($_SESSION['user_id'])) { ?>
-                <a href="/auth/logout.php">Logout</a>
+                <a href="../auth/logout.php">Logout</a>
             <?php } else { ?>
-                <a href="/auth/login.php">Login</a>
+                <a href="../auth/login.php">Login</a>
             <?php } ?>
         </nav>
     </div>
 </header>
-</body>
-</html>
